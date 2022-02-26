169306c1228d68c05424851df780220b04ff7584
AnSo <an.ragozina@gmail.com>
2022-02-26 18:46:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+<<<<<<< Updated upstream
 import Content from "./Components/Content"
 import Footer from "./Components/Footer"
 import Header from "./Components/Header"
+=======
+import Orders from "./Components/Orders";
+import Login from "./Pages/Login";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
       <div className="container d-flex flew-column flex-md-row">
         <Header />
         <div className="ps-0 ps-md-5 flex-grow-1">
@@ -12,6 +18,11 @@ function App() {
         </div>
       </div>
       <Footer />
+=======
+
+      <Orders />
+      <Login />
+>>>>>>> Stashed changes
     </div>
   )
 }
