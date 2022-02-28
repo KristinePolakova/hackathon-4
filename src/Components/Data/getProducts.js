47ba02d416f1345325c23578c51ea23896f701c7
Laura <Laura.stabulniece@gmail.com>
2022-02-28 14:29:56 +0200

    pagination; page-products; product line; product list; data-get products

---
+++ src/Components/Data/getProducts.js
@@ -0,0 +1,41 @@
+function getProducts() {
+    return [
+        {
+            id: 1,
+            image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fb8%2Ff9%2Fb8f9a23e3199fc61d98ed78667d0a727ed6c5afc.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bhome_porcelain%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]',
+            title: 'Plate - Porcelain 100%',
+            description: 'Porcelain plate. Fluted around the inside edge with a gold-coloured scalloped rim. Diameter 26 cm',
+            price: 10.00,
+        },
+        {
+            id: 2,
+            image : 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F40%2F90%2F4090fd80f89bb4288f99cf7dbeb497b05d0f96b6.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bhome_kitchen_ware%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B3%5D&call=url[file:/product/main]',
+            title: '3 pieces - fork, knife, spoon - Metal 100%',
+            description: 'Knife, fork and spoon in stainless steel. Length 20-22.5 cm',
+            price: 22.00,
+        },
+        {
+            id: 3,
+            image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fdf%2Fb3%2Fdfb3c794dd6722cc572006b529bf29673572a704.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bhome_glasswear%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]',
+            title: 'Champagne coupe - Glass 100%',
+            description: 'Champagne coupe in clear glass with a gold-coloured rim. Diameter at the top 10 cm, height 15.5 cm',
+            price: 5.00,
+        },
+        {
+            id: 4,
+            image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F16%2F97%2F16978818851fc8999babf0001c1f4aa0c2a3f841.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/fullscreen]',
+            title: 'Candlestick - Aluminium 100%',
+            description: 'Tall metal candlestick. Diameter approx. 2 cm. Height 33 cm',
+            price: 15.00,
+        },
+        {
+            id: 5,
+            image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fcf%2F19%2Fcf19faeb47dff214734a276865ec130ff6e95878.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/fullscreen]',
+            title: 'Table mat - Jute 100%',
+            description: 'Round, hole-patterned table mat in braided jute',
+            price: 3.00,
+        }
+    ]
+}
+
+export default getProducts
---
+++ src/Components/ProductList.js
@@ -0,0 +1,99 @@
+import { useState } from "react"
+import ProductLine from "./ProductLine"
+import getProducts from "./Data/getProducts"
+
+function ProductList() {
+    const [items, setItems] = useState(getProducts())
+
+    const deleteItem = (itemIndex) => {
+        items.splice(itemIndex, 1)
+        setItems([...items])
+    }
+    const [startList, setStartList] = useState(0)
+    const endList = startList + 5
+
+    const moveLeft = () => {
+        let nextSelected = startList - 5
+        if (nextSelected < 0) {
+            nextSelected = 0
+        }
+        setStartList(nextSelected)
+    }
+    const moveRight = () => {
+        let nextSelected = startList + 5
+        if (nextSelected > 15) {
+            nextSelected = 15
+        }
+        setStartList(nextSelected)
+    }
+    const changePageOne = () => {
+        setStartList(0)
+    }
+    const changePageTwo = () => {
+        setStartList(5)
+    }
+    const changePageThree = () => {
+        setStartList(10)
+    }
+    const changePageFour = () => {
+        setStartList(15)
+    }
+    const changePageFive = () => {
+        setStartList(20)
+    }
+
+    const itemsList = items.slice(startList, endList).map((item, image, id, title, description, price, itemDiscount, index) => {
+
+        return (
+            <ProductLine
+                key={image}
+                item={item}
+                src={image}
+                id={id}
+                title={title}
+                description={description}
+                price={price}
+                itemDiscount={itemDiscount}
+                deleteItem={deleteItem}
+                index={index}
+            />
+        )
+    })
+
+    return (
+        <div>
+            <div>
+                <table className="table border my-3">
+                    <thead>
+                        <tr>
+                            <th scope="col" className="align-middle text-center">Image</th>
+                            <th scope="col" className="align-middle text-center">ID</th>
+                            <th scope="col" className="align-middle text-center">Title</th>
+                            <th scope="col" className="align-middle text-center">Description</th>
+                            <th scope="col" className="align-middle text-center">Price</th>
+                            <th scope="col" className="align-middle text-center">Discount price</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>{itemsList}</tbody>
+                </table>
+                <div className="row">
+                    <div className="col">
+                        <div className="btn-group float-end" role="group" aria-label="Basic outlined example">
+                            <button onClick={() => moveLeft(startList)} type="button" class="btn btn-outline-secondary">«</button>
+                            <button onClick={() => changePageOne(startList)} type="button" class="btn btn-outline-secondary">1</button>
+                            <button onClick={() => changePageTwo(startList)} type="button" class="btn btn-outline-secondary">2</button>
+                            <button onClick={() => changePageThree(startList)} type="button" class="btn btn-outline-secondary">3</button>
+                            <button onClick={() => changePageFour(startList)} type="button" class="btn btn-outline-secondary">4</button>
+                            <button onClick={() => changePageFive(startList)} type="button" class="btn btn-outline-secondary">5</button>
+                            <button onClick={() => moveRight(startList)} type="button" class="btn btn-outline-secondary">»</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
