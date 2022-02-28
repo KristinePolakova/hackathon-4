b5dd7bb8b82ccdf2cf28fa37af992e653b2ee890
Laura <Laura.stabulniece@gmail.com>
2022-03-01 01:14:55 +0200

    pagination update, table update, delete button

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -14,7 +14,9 @@ function Products() {
                     </div>
                 </div>
             </div>
+            <div style={{ marginBottom: '40px' }}>
                 <ProductsSearch />
+            </div>
             <div>
                 <ProductList />
             </div>
